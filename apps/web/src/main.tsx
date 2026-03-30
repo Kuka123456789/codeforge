ac209b11ba8c9c3e137f4c68ce70b6b2957b6368
Daniel Odesser <danielodesser@MacBookPro.lan>
2026-03-30 20:42:10 +0300

    auto: 3 files changed, 11 insertions(+), 18 deletions(-)

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
 import { createHashHistory, createBrowserHistory } from "@tanstack/react-router";
+import { Analytics } from "@vercel/analytics/react";
 
 import "@xterm/xterm/css/xterm.css";
 import "./index.css";
@@ -20,5 +21,6 @@ document.title = APP_DISPLAY_NAME;
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <Analytics />
   </React.StrictMode>,
 );
